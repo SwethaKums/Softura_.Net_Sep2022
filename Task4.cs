b8d36bea16acf3b69582448cdbd8275dd7e574f4
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-09-07 15:35:03 +0530

    Add files via upload

---
+++ Task4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{ 
+    internal class Task4
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i<=number && number<=20; i++)
+                Console.WriteLine(("N"+"*"+i+"="+i+"N"));
+        }
+    }
+}
---
+++ Task5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Task5
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter the package");
+            String package= Console.ReadLine();
+            String[] packageA = { "South special ", "Childrrens club", "Movies" };
+            for(int i = 0; i<packageA.Length; i++)
+            {
+                Console.WriteLine(packageA[i]);
+            }
+
+            String[] packageB = { "News ","sports", "Movies" ,"Regional2"};
+            for (int i = 0; i<packageB.Length; i++)
+            {
+                Console.WriteLine(packageB[i]);
+            }
+            String[] packageC = { "Dicovery","history","national" };
+            for (int i = 0; i<packageC.Length; i++)
+            {
+                Console.WriteLine(packageC[i]);
+            }
+            switch(package)
+            {
+                case "packageA":Console.WriteLine("the amount is{0}", 250);
+                    break;
+                case "packageB":Console.WriteLine("the amount is{0}", 450);
+                    break; 
+                case "packageC":Console.WriteLine("the amount is{0}", 350);
+                    break;
+                default:Console.WriteLine("Enter the correct package");
+                    break;
+
+                    
+            }
+
+            
+        }
+    }
+}
