bb4af6722e2afc9ddc5643b9c01e31c77e549bf5
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-09-09 07:01:39 +0530

    Add files via upload

---
+++ Membership.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Membership
+    {
+        public static void Main()
+        {
+Membership membership = new Membership();
+            membership.userLogin("swetha", "swetha@31");
+            membership.userLogin(001, 678);
+            membership.userLogin(9789459874L, 897);
+        }
+        public void userLogin(String emailid,string password)
+        {
+            Console.WriteLine("the emailis is {0} and password is {1}",emailid,password);
+        }
+        public void userLogin(int memberid, int pin)
+        {
+            Console.WriteLine("the memberid is {0} and the pin number  is {1}",memberid,pin);
+        }
+        public void userLogin(long mobilenumber, int pin)
+        {
+            Console.WriteLine("the mobilenumber is {0} and the pin is {1}",mobilenumber,pin);
+        }
+    }
+}
---
+++ Operators.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Operators
+    {
+        
+        public static void Main()
+        {
+
+            MulDiv mulDiv = new MulDiv();
+            mulDiv.multiply(5, 7);
+            mulDiv.divide(7, 9);
+            AddSub addSub = new AddSub();
+            addSub.add(8, 9);
+            addSub.subtraction(9, 10);
+
+
+
+        }
+        public class AddSub
+        {
+            int n1, n2;
+            public void add(int n1, int n2)
+            {
+
+                this.n1 = n1;
+                this.n2 = n2;
+                Console.WriteLine("The addition of two number is{0}",n1+n2);
+
+            }
+
+            public void subtraction(int n1, int n2)
+            {
+
+                this.n1 = n1;
+                this.n2 = n2;
+                Console.WriteLine("The sub of two number is {0}",n1-n2);
+
+            }
+        }
+        public class MulDiv:AddSub
+
+        {
+            int n1, n2;
+            public void multiply(int n1, int n2)
+            {
+
+                this.n1 = n1;
+                this.n2 = n2;
+                Console.WriteLine("The addition of multiplication number is  {0}",n1*n2);
+
+            }
+
+            public void divide(int n1, int n2)
+            {
+
+                this.n1 = n1;
+                this.n2 = n2;
+                Console.WriteLine("The divison of two number is {0}",n1/n2);
+
+            }
+            
+        }
+    }
+}
