20bb84f02eb6babf98d1b8f71732e7362cf2baaa
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-09-09 18:29:02 +0530

    Add files via upload

---
+++ Tax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Tax
+    {
+        public static void Main()
+        {
+         Vat vat = new Vat();
+            vat.vatCalculation(500);
+            Gst gst = new Gst();
+            gst.vatCalculation(500);
+        }
+
+      class Vat
+        {
+            public void vatCalculation(int amount)
+            {
+               
+                Console.WriteLine("the vat is{0}",(amount*12)/100);
+            }
+        }
+        class Gst:Vat
+        {
+            public  new void vatCalculation(int amount)
+            {
+                
+                Console.WriteLine("the gst is{0}",(amount*20)/100);
+            }
+        }
+    }
+}
