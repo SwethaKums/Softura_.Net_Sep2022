97090b50814a416cc3a46a60ad713c5f8d0c109e
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-10-03 15:29:40 +0530

    Add files via upload

---
+++ MvcTask2/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult GetAllInfo()
+        {
+            List<EmployeeDetails> listemp = EmployeeDetails.GetEmployeeDetails();
+            return View(listemp);
+
+        }
+        public IActionResult GetInfo()
+        {
+            EmployeeDetails emp = new EmployeeDetails() { EmpId=5, EmpFirstName="sobana", EmpLastName="Babu", EmpGender="female" };
+           
+
+            return View(emp);
+
+
+        }
+
+        public IActionResult Insert()
+        {
+
+            return View();
+        }
+
+    }
+
+}
---
+++ MvcTask2/EmployeeDetails.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeApp.Models
+{
+    public class EmployeeDetails
+    {
+        public int EmpId { get; set; }
+    
+        public string EmpFirstName { get; set; }
+
+        public string EmpLastName { get; set; }
+
+        public string EmpGender { get; set; }
+
+
+        public static List<EmployeeDetails> GetEmployeeDetails()
+        {
+            List<EmployeeDetails> empDetails = new List<EmployeeDetails>()
+            {
+                new EmployeeDetails(){EmpId=1,EmpFirstName="swetha",EmpLastName="kumar",EmpGender="female"},
+                new EmployeeDetails(){EmpId=2,EmpFirstName="divya",EmpLastName="venkat",EmpGender="female"},
+                new EmployeeDetails(){EmpId=3,EmpFirstName="sandhya",EmpLastName="venkat",EmpGender="female"},
+                new EmployeeDetails(){EmpId=4,EmpFirstName="sumithra",EmpLastName="babu",EmpGender="female"}
+            };
+            return empDetails;
+        }
+    }
+}
